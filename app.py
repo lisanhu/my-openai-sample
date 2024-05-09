b393f80eafa58c011135d86f854ab73ec81633f5
Sanhu Li <lisanhu2014@hotmail.com>
2024-05-09 08:29:41 -0400

    fixing an issue due to openai update

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, request, jsonify, send_from_directory
-import openai
+from openai import OpenAI
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
@@ -9,7 +9,8 @@ load_dotenv()
 app = Flask(__name__)
 
 # Get the OpenAI API key from the environment variable
-openai.api_key = os.environ["OPENAI_API_KEY"]
+OpenAI.api_key = os.environ["OPENAI_API_KEY"]
+
 
 # Serve index.html file from the root
 @app.route("/")
@@ -24,13 +25,18 @@ def generate_response():
 
     messages = [{"role": "system", "content": system_message}] + conversation
 
-    response = openai.ChatCompletion.create(
-        model="gpt-4-0314",
+    client = OpenAI(
+        api_key=os.environ.get("OPENAI_API_KEY"),
+    )
+    # model="gpt-4-0314",
+    response = client.chat.completions.create(
+        model="gpt-4-turbo",
         messages=messages,
     )
 
     message = response.choices[0].message.content.strip()
     return jsonify({"botResponse": message})
+
 
 if __name__ == "__main__":
     app.run(port=8080, debug=False)
